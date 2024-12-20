9a0829e3c807d8d2ed982e89e0b888892c895bec
zaineb.bachouch <zaineb.bachouch@esprit.tn>
2024-12-20 23:44:12 +0100

    Configured database for unified testing and production

--- crmBackend/scripts/init.sql
+++ crmBackend/scripts/init.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE IF NOT EXISTS crm_db;
+USE crm_db;
+
 CREATE TABLE IF NOT EXISTS categorie (
     idcategorie INT AUTO_INCREMENT PRIMARY KEY,
     nom_categorie VARCHAR(255) NOT NULL,
