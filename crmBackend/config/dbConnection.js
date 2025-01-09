5b04fcce9194cbe08412ae4e15ca3a991fb51e3c
zaineb.bachouch <zaineb.bachouch@esprit.tn>
2025-01-09 13:44:14 +0100

    Track large files with Git LFS and add project files

--- crmBackend/config/dbConnection.js
+++ crmBackend/config/dbConnection.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || '127.0.0.1',
     user: process.env.DB_USERNAME || 'root',
     password: process.env.DB_PASSWORD || 'root123',
     database: process.env.DB_NAME || 'crm_db',
